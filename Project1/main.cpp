c067309b4b6607134675eada92683f03fa69574d
KharytonchykRaman <157610424+KharytonchykRaman@users.noreply.github.com>
2024-03-20 18:28:21 +0300

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,11 +7,39 @@ class BankAccount {
 public:
 	string name;
 	int balance;
+
+	string getInfo() {
+		string info = "Name: " + name + ", balance: " + to_string(balance);
+		return info;
+	}
+
+	void withdraw(int amount) {
+		if (amount <= balance)
+		{
+			balance -= amount;
+		}
+	}
+
+	void deposit(int amount) {
+		balance += amount;
+	}
 private:
 	//some code 
 protected:
 	//some code
 };
+
+string get_best_account_name(BankAccount* accounts, int size) {
+	BankAccount max = accounts[0];
+
+	for (int i = 1; i < size; i++)
+	{
+		if (max.balance < accounts[i].balance) {
+			max = accounts[i];
+		}
+	}
+	return max.name;
+}
 
 int main() {
 
@@ -24,16 +53,15 @@ int main() {
 	accounts[1].name = "87654321";
 	accounts[1].balance = 5000;
 
-	BankAccount max = accounts[0];
+	accounts[0].deposit(5000);
+	accounts[1].withdraw(15000);
 
-	for (int i = 1; i < size; i++)
+	cout << get_best_account_name(accounts, size) << endl;
+
+	for (int i = 0; i < size; i++)
 	{
-		if (max.balance < accounts[i].balance) {
-			max = accounts[i];
-		}
+		cout << accounts[i].getInfo() << endl;
 	}
-
-	cout << max.name;
 
 	return 0;
 }
